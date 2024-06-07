6e3c7cf11ca9ef8c2f29bd508340757f9abc7078
OhijMik <jihok148@gmail.com>
2024-06-07 16:59:53 -0400

    Added movement to the eagle
    - Added movement to the eagle so that it goes towards the player when the player is in a certain range.

--- eagle.gd
+++ eagle.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
+const ATTACK_SPEED = 300.0
 @onready var anim = get_node("AnimationPlayer")
+var follow = false
+var player
 
 
 func _ready():
@@ -10,5 +13,32 @@ func _ready():
 
 
 func _physics_process(delta):
+	if follow:
+		var direction = (player.position - position).normalized()
+		
+		velocity.x = direction.x * ATTACK_SPEED
+		velocity.y = direction.y * ATTACK_SPEED
+		
+		if velocity.x > 0:
+			get_node("AnimatedSprite2D").flip_h = true
+		else:
+			get_node("AnimatedSprite2D").flip_h = false
+	else:
+		velocity.x = -1 * SPEED
 	
 	move_and_slide()
+
+
+func _on_range_body_entered(body):
+	if body.name == "Player":
+		var direction = (body.position - position).normalized()
+		velocity.x = direction.x * 300
+		velocity.y = direction.y * 300
+		player = body
+		follow = true
+
+
+func _on_hitbox_body_entered(body):
+	if body.name == "Player":
+		queue_free()
+		body.queue_free()
